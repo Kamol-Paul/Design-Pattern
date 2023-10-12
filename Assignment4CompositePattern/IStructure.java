78e005f2eaf1a39c5135522d931be17cf0a3cca6
Kamol-Paul <s1910176121@ru.ac.bd>
2023-10-13 02:33:14 +0600

    composite pattern done

---
+++ Assignment4CompositePattern/IStructure.java
@@ -0,0 +1,8 @@
+package Assignment4CompositePattern;
+
+public interface IStructure {
+    public void enter();
+    public void exit();
+    public void location();
+    public String getName();
+}
